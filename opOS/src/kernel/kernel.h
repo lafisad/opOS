a9a4e0962c23e6cf71f7fae5f401f5328c5496a9
txhc <fantahustensaft@tuta.io>
2025-08-25 18:49:44 +0200

    asd

--- opOS/src/kernel/kernel.h
+++ opOS/src/kernel/kernel.h
@@ -1,16 +1,25 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#include "terminal.h"
 
-// Kernel version information
-#define KERNEL_NAME    "opOS"
-#define KERNEL_VERSION "0.1.0"
+#include <stdint.h>
 
-// Function declarations
-void kernel_main(unsigned long magic, unsigned long addr);
 
-// Panic function for unrecoverable errors
+
+
+// Kernel entry point
+void kernel_main(void);
+
+
+
+// Early initialization
+void early_init(void);
+
+
+// Main kernel initialization
+void kernel_init(void);
+
+// Panic handler
 void panic(const char* message);
 
 #endif // KERNEL_H
